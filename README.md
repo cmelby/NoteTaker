b5c1a08f6908f8de18af0dd83ab1395ad263d015
Christopher Melby <melby.christopher@gmail.com>
2020-01-14 20:45:39 -0800

    organizing files for better readability

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # NoteTaker
 
+## Summary 
+For users that need to keep track of a lot of information, tasks or goals, it's easy to forget or loose track of important next steps. Being able to take persistent notes allows users to have written information available when needed to reference these important pieces of information. NoteTaker does just this, allows users to take notes via a CLI application that launches in the browser. After launching the application you can keep a tab open in the browser, take and edit notes, and trash the ones that you have marked off your list. Just follow the instructions below to get started.
+
+
 ## Demo
 ![Site](Assets/siteDemo.gif) 
 
-## Instructions
+## Getting Started
 
+### Instructions:
 There are a couple dependencies that are have already been provided for you in the package json. All you need to do is open your terminal and run "npm install". This gives you access to the packages that will run the applicaiton, mainly the express.js package. Next, in terminal run "node server.js". This will launched your local host 3000. You can then navigate to your browser and go to https://localhost:3000 where you will be able to start taking and editing notes.
 
 
@@ -20,6 +25,7 @@ There are a couple dependencies that are have already been provided for you in t
 - GitHub - Hosts repository that can be deployed to GitHub Pages
  
 ## Code Snippet
+The code snippet below handles the request to delete a note. Calling on the built in method in express "delete("/") we direct to the api path where we can get the id's that have been set to individual notes in our database. We then read the files from that directory, parse out the data that we need in this case the id, then splice that id out of the database object when the note has been deleted. Once that work has been done we still need to display these changes, we we write the updated file from our database back to the browser.
 
 ```js
 app.delete('/api/notes/:id', function (req, res) {
--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const express = require("./node_modules/express");
 const path = require("path");
 const fs = require("fs");
 
-//Instantiating exprees and port for heroku...
+//Instantiating exprees and port for heroku enviroment...
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+//Setup express.static to access 'public' files....
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
-//============ HTML ROUTES ======================//
+//============ HTML ROUTE =================================//
 
 //GET /notes - returns the notes.html file
 app.get('/notes', function(req, res) {
@@ -22,71 +23,68 @@ app.get('/notes', function(req, res) {
 });
 
 
-//============ API ROUTES======================//
-
+//============ API ROUTES=============================================//
 //Reading files from the db.json file and return all saved notes as JSON....
 app.get("/api/notes", function(req, res) {
   fs.readFile("db/db.json", 'utf8', function(err, data) {
     if (err) throw err
-    var newNote = JSON.parse(data);
-    res.json(newNote);
+      var newNote = JSON.parse(data);
+      res.json(newNote);
     })
     
   });
 
-// GET * - Return the index.html file
+// GET * - Returns the index.html file
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-//===========================DELETE 1============================================//
+//===========================DELETE====================================================//
 // DELETE /api/notes/:id - Recives a query paramter containing the id of a note to delete.
 // and then rewrite the notes to the db.json file.
 app.delete('/api/notes/:id', function (req, res) {
   
-  var chosenNote = req.params.id
-  console.log(chosenNote)
+  var emptyNote = req.params.id
+  console.log(emptyNote)
 
   fs.readFile("db/db.json", "utf8", function (err, data) {
       console.log('delet1')
       if (err) throw err
 
-      var note = JSON.parse(data)
-      var index = parseInt(chosenNote) - 1
-      note.splice(index, 1);
+      var noteDelete = JSON.parse(data)
+      var index = parseInt(emptyNote) - 1
+        noteDelete.splice(index, 1);
 
-      fs.writeFile('db/db.json', JSON.stringify(note), 'utf8', function (err) {
-      if (err) throw err
-      console.log('deletedone2')
+        fs.writeFile('db/db.json', JSON.stringify(noteDelete), 'utf8', function (err) {
+        if (err) throw err
+        console.log('deletedone2')
       })
   })
-  res.send(chosenNote)
+  res.send(emptyNote)
 });
 
-//============ Write to file using the fs module ======//
-//POST /api/notes - Should recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client....
+//============ Write to file using the fs module ==============================================//
+//POST /api/notes - Recieves a new note to save on the request body, adds it to the db.json file, 
+// and then returns the new note to the client....
 app.post("/api/notes", function(req, res){
   var newNote = req.body
   console.log(newNote)
   fs.readFile("db/db.json", "utf8", function(err, data){
     console.log('done1')
-    
         if (err) throw err
-        var note = JSON.parse(data)
+        var noteInfo = JSON.parse(data)
 
-        note.push(newNote)
-        note.forEach((item, i) => item.id = i + 1);
-        console.log(note)
+        noteInfo.push(newNote)
+        noteInfo.forEach((item, i) => item.id = i + 1);
+        console.log(noteInfo)
 
-         fs.writeFile('db/db.json', JSON.stringify(note), 'utf8', function(err){
+         fs.writeFile('db/db.json', JSON.stringify(noteInfo), 'utf8', function(err){
          if(err) throw err
          console.log('done2')
      })
   })
   res.json(newNote)
 });
-
-
 
 
 //launch port...............................
